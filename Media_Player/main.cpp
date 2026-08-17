14102e8fe10474aa8c6c15f4b64123db41bfd59e
Alyaa-Abdelmoniem <aliaelshetry1@gmail.com>
2026-08-17 21:28:07 +0300

    main.cpp

---
+++ Media_Player/main.cpp
@@ -0,0 +1,25 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() {
+            QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection
+        );
+
+    engine.loadFromModule("media_player", "Main");
+
+    return app.exec();
+}
